26bb8c55f96472b829f04da26e566ba786fbcdc6
huyen.anh <huyen.anh.fe@gmail.com>
2023-08-30 17:18:34 +0700

    [Update] add user-input-Width/Height

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,26 @@
     <button @click="onSelectedType('bottom')" :class="typeIsSelected === 'bottom' ? 'is-selected' : ''">bottom</button>
     <button @click="onSelectedType('left')" :class="typeIsSelected === 'left' ? 'is-selected' : ''">left</button>
     <br>
-    <button @click="onSelectedType('top-left')" :class="typeIsSelected === 'top-left' ? 'is-selected' : ''">top-left</button>
-    <button @click="onSelectedType('top-right')" :class="typeIsSelected === 'top-right' ? 'is-selected' : ''">top-right</button>
-    <button @click="onSelectedType('bottom-left')" :class="typeIsSelected === 'bottom-left' ? 'is-selected' : ''">bottom-left</button>
-    <button @click="onSelectedType('bottom-right')" :class="typeIsSelected === 'bottom-right' ? 'is-selected' : ''">bottom-right</button>
+    <button @click="onSelectedType('top-left')"
+      :class="typeIsSelected === 'top-left' ? 'is-selected' : ''">top-left</button>
+    <button @click="onSelectedType('top-right')"
+      :class="typeIsSelected === 'top-right' ? 'is-selected' : ''">top-right</button>
+    <button @click="onSelectedType('bottom-left')"
+      :class="typeIsSelected === 'bottom-left' ? 'is-selected' : ''">bottom-left</button>
+    <button @click="onSelectedType('bottom-right')"
+      :class="typeIsSelected === 'bottom-right' ? 'is-selected' : ''">bottom-right</button>
+    <div class="form-control" :class="{ invalid: !width.isValid }">
+      <label for="width">Width:</label>
+      <input type="number" id="width"
+        v-model.number="width.val" @blur="clearValidity(width)">
+      <p class="text-error" v-if="!width.isValid">Width must be greater than 0.</p>
+    </div>
+    <div class="form-control" :class="{ invalid: !height.isValid }">
+      <label for="height">Height:</label>
+      <input type="number" id="height"
+        v-model.number="height.val" @blur="clearValidity(height)">
+      <p class="text-error" v-if="!height.isValid">Height must be greater than 0.</p>
+    </div>
   </div>
   <hr>
   <div class="box-result" :style="styleCSS"></div>
@@ -30,68 +46,91 @@ export default {
         'height: 0',
         'border-style: solid',
       ],
-      width: 0,
-      height: 0,
+      width: {
+        val: '',
+        isValid: true,
+      },
+      height: {
+        val: '',
+        isValid: true,
+      },
       styles: null,
       styleCSS: '',
       typeIsSelected: '',
+      formIsValid: true,
     }
   },
   methods: {
+    clearValidity(input) {
+      input.isValid = true;
+    },
     onSelectedType(type) {
-      this.width = 30;
-      this.height = 30;
-      const halfWidth = this.width / 2;
-      const halfHeight = this.height / 2;
-      
+      //validate input values
+      this.formIsValid = true;
+      if ( !this.width.val || this.width.val < 0 ) {
+        this.width.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.height.val || this.height.val < 0) {
+        this.height.isValid = false;
+        this.formIsValid = false;
+      }
+
+      //setup value for triangle
+      const width = this.width.val;
+      const height = this.height.val;
+      const halfWidth = width / 2;
+      const halfHeight = height / 2;
+
       this.typeIsSelected = type;
+
       //Count value and generate source
       switch (type) {
         case 'top':
           this.styles = [...this.styleDefault].concat([
-            `border-width: 0 ${halfWidth}px ${this.height}px ${halfWidth}px`,
+            `border-width: 0 ${halfWidth}px ${height}px ${halfWidth}px`,
             `border-color: transparent transparent #007bff transparent`,
           ]);
           break;
         case 'right':
           this.styles = [...this.styleDefault].concat([
-            `border-width: ${halfHeight}px 0 ${halfHeight}px ${this.width}px`,
+            `border-width: ${halfHeight}px 0 ${halfHeight}px ${width}px`,
             `border-color: transparent transparent transparent #007bff`,
           ]);
           break;
         case 'bottom':
           this.styles = [...this.styleDefault].concat([
-            `border-width: ${this.height}px ${halfWidth}px 0 ${halfWidth}px`,
+            `border-width: ${height}px ${halfWidth}px 0 ${halfWidth}px`,
             `border-color: #007bff transparent transparent transparent`,
           ]);
           break;
         case 'left':
           this.styles = [...this.styleDefault].concat([
-            `border-width: ${halfHeight}px ${this.width}px ${halfHeight}px 0`,
+            `border-width: ${halfHeight}px ${width}px ${halfHeight}px 0`,
             `border-color: transparent #007bff transparent transparent`,
           ]);
           break;
         case 'top-left':
           this.styles = [...this.styleDefault].concat([
-            `border-width: ${this.height}px ${this.width}px 0 0`,
+            `border-width: ${height}px ${width}px 0 0`,
             `border-color: #007bff transparent transparent transparent`,
           ]);
           break;
         case 'top-right':
           this.styles = [...this.styleDefault].concat([
-            `border-width: 0 ${this.width}px ${this.height}px 0`,
+            `border-width: 0 ${width}px ${height}px 0`,
             `border-color: transparent #007bff transparent transparent`,
           ]);
           break;
         case 'bottom-left':
           this.styles = [...this.styleDefault].concat([
-            `border-width: ${this.height}px 0 0 ${this.width}px`,
+            `border-width: ${height}px 0 0 ${width}px`,
             `border-color: transparent transparent transparent #007bff`,
           ]);
           break;
         case 'bottom-right':
           this.styles = [...this.styleDefault].concat([
-            `border-width: 0 0 ${this.height}px ${this.width}px`,
+            `border-width: 0 0 ${height}px ${width}px`,
             `border-color: transparent transparent #007bff transparent`,
           ]);
           break;
@@ -121,11 +160,21 @@ export default {
 }
 
 button.is-selected {
-  color: red;
+  color: blueviolet;
 }
 
 .box-generated-css {
   text-align: left;
   font-family: monospace, sans-serif;
 }
+
+.form-control.invalid input {
+  border: 1px solid red;
+  color: red;
+}
+
+.text-error {
+  color: red;
+}
+
 </style>
